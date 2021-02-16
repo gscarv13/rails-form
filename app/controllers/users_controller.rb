08fe2b54487009af55756b33c3ecacbb61d532c4
aliciapaz <aliciapazrojas@gmail.com>
2021-02-16 12:49:47 -0300

    create form using form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    @user = User.new(user_params)
-
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+  
     if @user.save
       redirect_to new_user_path
     else
@@ -11,10 +11,20 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+  # def create
+  #   @user = User.new(user_params)
 
-  def user_params
-    params.require(:user).permit(:username, :email, :password) 
-  end
+  #   if @user.save
+  #     redirect_to new_user_path
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # private
+
+  # def user_params
+  #   params.require(:user).permit(:username, :email, :password) 
+  # end
 
 end
