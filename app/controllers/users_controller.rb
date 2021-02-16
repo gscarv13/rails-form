5f534e8723935ce220eeeab46d48f736d762ea84
Gustavo Carvalho <gsc13@tuta.io>
2021-02-16 13:25:45 -0300

    Add form_with tags to user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,10 @@
 class UsersController < ApplicationController
-  def new; end
-
-  def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-  
-    if @user.save
-      redirect_to new_user_path
-    else
-      render :new
-    end
+  def new
+    @user = User.new
   end
 
   # def create
-  #   @user = User.new(user_params)
+  #   @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
   #   if @user.save
   #     redirect_to new_user_path
@@ -21,10 +13,20 @@ class UsersController < ApplicationController
   #   end
   # end
 
-  # private
+  def create
+    @user = User.new(user_params)
 
-  # def user_params
-  #   params.require(:user).permit(:username, :email, :password) 
-  # end
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password) 
+  end
 
 end
